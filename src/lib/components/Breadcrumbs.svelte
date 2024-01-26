ceec50dcdebe3b83f2e94fa642d874f029852fea
Zac Holland <zacharyholland@gmail.com>
2024-01-25 19:21:49 -0800

    use relative path in glob import

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -20,7 +20,7 @@
     // If nothing is passed to routeModules, populate it
     if (routeModules === undefined) {
       routeModules = {};
-      const _routeModules = import.meta.glob("/src/routes/**/*.svelte");
+      const _routeModules = import.meta.glob("./src/routes/**/*.svelte");
       for (const [key, value] of Object.entries(_routeModules)) {
         const module = (await value()) as ModuleData;
         // Only store the info we need to cut down on memory usage
